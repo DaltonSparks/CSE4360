3d621f7db2bf24d056f75ef56c2954d0d19cd7c3
Dalton <dalton.lee.sparks@gmail.com>
2022-10-24 17:01:27 -0500

    first

---
+++ main.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import math as math
+
+#define MAX_OBSTACLES 25 /* maximum number of obstacles */
+num_obstacles = 13 #number of obstacles
+obstaclegrid = [
+(0.61, 2.743),(0.915, 2.743),(1.219, 2.743),(1.829, 1.219),
+(1.829, 1.524),( 1.829, 1.829), (1.829, 2.134),(2.743, 0.305),
+(2.743, 0.61),(2.743, 0.915),(2.743, 2.743),(3.048, 2.743),
+(3.353, 2.743),
+(-1,-1),(-1,-1),(-1,-1),(-1,-1),(-1,-1),(-1,-1),(-1,-1),(-1,-1),(-1,-1),
+(-1,-1),(-1,-1),(-1,-1),
+]
+start = (0.305, 1.219)
+goal = (3.658, 1.829)
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+#connected motors to ports
+LM = Motor(Port.A)
+RM = Motor(Port.B)
+
+#speed variables
+# 16x10 12 inch tiles
+#move speed and time
+MS = ((305/(69*math.pi))*360)/2
+MT = 1500
+#turn speed and time
+TS = MS/2
+TT = 900
+
+def left():
+    LM.run_time(-TS,TT,Stop.HOLD,False)
+    RM.run_time(TS,TT,Stop.HOLD,True)
+
+def right():
+    LM.run_time(TS,TT,Stop.HOLD,False)
+    RM.run_time(-TS,TT,Stop.HOLD,True)
+
+def forward():
+    LM.run_time(MS,MT,Stop.HOLD,False)
+    RM.run_time(MS,MT,Stop.HOLD,True)
+
+def backward():
+    LM.run_time(-MS,MT,Stop.HOLD,False)
+    RM.run_time(-MS,MT,Stop.HOLD,True)
+
+#testing movement
+left()
+left()
+right()
+right()
+forward()
+backward()
+
+#construct workspace with all paths and obstacles 
+
+#implement a function to calculate path to take and movements
+
+
+# Write your program here.
+ev3.speaker.beep()
