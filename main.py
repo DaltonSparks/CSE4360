451206aa406087ea29907ff6c19be29fb1a1e281
Dalton <dalton.lee.sparks@gmail.com>
2022-10-25 17:10:35 -0500

    movment implemented L,R,F,B

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 import math as math
-
+import time
 #define MAX_OBSTACLES 25 /* maximum number of obstacles */
 num_obstacles = 13 #number of obstacles
 obstaclegrid = [
@@ -28,15 +28,16 @@ ev3 = EV3Brick()
 #connected motors to ports
 LM = Motor(Port.A)
 RM = Motor(Port.B)
-
+G = GyroSensor(Port.s1)
 #speed variables
 # 16x10 12 inch tiles
-#move speed and time
-MS = ((305/(69*math.pi))*360)/2
-MT = 1500
+#move speed and time 
+MS = ((305/(85*math.pi))*360)/2
+MT = 1480*2
 #turn speed and time
-TS = MS/2
-TT = 900
+TS = 150 #((305/(85*math.pi))*360)/2
+print(TS)
+TT = 1165 #890
 
 def left():
     LM.run_time(-TS,TT,Stop.HOLD,False)
@@ -53,14 +54,33 @@ def forward():
 def backward():
     LM.run_time(-MS,MT,Stop.HOLD,False)
     RM.run_time(-MS,MT,Stop.HOLD,True)
+c = 0
+while(c<5):
 
 #testing movement
-left()
-left()
-right()
-right()
-forward()
-backward()
+    time.sleep(1)
+    left()
+#left()
+#time.sleep(1)
+#right()
+#right()
+    time.sleep(1)
+    forward()
+#backward()
+    time.sleep(1)
+    left()
+    time.sleep(1)
+    forward()
+    time.sleep(1)
+    left()
+    time.sleep(1)
+    forward()
+    time.sleep(1)
+    left()
+    time.sleep(1)
+    forward()
+    c+=1
+
 
 #construct workspace with all paths and obstacles 
 
@@ -68,4 +88,4 @@ backward()
 
 
 # Write your program here.
-ev3.speaker.beep()
+#ev3.speaker.beep()
